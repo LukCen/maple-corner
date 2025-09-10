eb922997b645f47adf470c3dbb775f56d99b08fd
lbjcena <lbjcena@gmail.com>
2025-09-10 20:31:01 +0200

    desktop menu added - basic layout and items

--- app/components/MainMenu.vue
+++ app/components/MainMenu.vue
@@ -10,18 +10,22 @@ function saveClick() {
 }
 
 // list of stuff sold - drinks, confectionaries, desserts, extras etc -, child of 'Menu' element from array above
-// const itemsList = []
+const desktopMenuItems = [
+  { id: 1, title: 'Item 1', icon: 'lucide:dice-1' },
+  { id: 2, title: 'Item 2', icon: 'lucide:dice-2' },
+  { id: 3, title: 'Item 3', icon: 'lucide:dice-3' },
+  { id: 4, title: 'Item 4', icon: 'lucide:dice-4' },
+]
 </script>
 <template>
   <!-- mobile/tablet -->
-  <nav class="flex fixed z-10 bottom-0 gap-4 w-11/12 m-[0_auto] justify-around items-center min-h-[60px] rounded-xl">
+  <nav class="flex desktop:hidden fixed z-10 bottom-0 gap-4 w-11/12 m-[0_auto] justify-around items-center min-h-[60px] rounded-xl">
     <ul class="--menu-left flex gap-1 flex-1">
       <BaseButton class="bg-espresso rounded-md text-beige">Item</BaseButton>
       <BaseButton class="bg-espresso rounded-md text-beige">Item</BaseButton>
     </ul>
 
-    <NuxtImg width="50" height="50" src="/maplecorner_logo.webp"
-      class="rounded-full shadow-[0px_0px_10px_1px_var(--color-beige)]" />
+    <NuxtImg width="50" height="50" src="/maplecorner_logo.webp" class="rounded-full shadow-[0px_0px_10px_1px_var(--color-beige)]" />
 
     <ul class="--menu-right flex gap-1 flex-1">
       <BaseButton class="bg-espresso rounded-md text-beige">Item</BaseButton>
@@ -30,6 +34,21 @@ function saveClick() {
       </BaseButton>
     </ul>
   </nav>
+
+  <!-- desktop -->
+  <nav class="hidden desktop:flex z-10 gap-4 w-full items-center min-h-[60px] rounded-xl bg-espresso/95 text-beige px-2 py-1 shadow-[0px_0px_5px_1px_var(--color-beige)]">
+    <div class="logo_text flex gap-2 items-center">
+      <NuxtImg src="/maplecorner_logo.webp" class="rounded-full" />
+      <span class="text-4xl">Maple Corner</span>
+    </div>
+    <ul class="flex gap-1 flex-1 justify-around items-center">
+      <li v-for="item in desktopMenuItems" :key="item.id" class="flex items-center justify-center">
+        <Icon :name="item.icon" size="45" />
+        <span>{{ item.title }}</span>
+      </li>
+    </ul>
+  </nav>
+
 
 
 </template>

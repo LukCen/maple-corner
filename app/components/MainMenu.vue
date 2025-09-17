7f7edb4967180446d31949ead6d6b68b88672dbe
lbjcena <lbjcena@gmail.com>
2025-09-17 02:07:22 +0200

    layout improvements - moved to grid for menu page, top navbar replaced with proper links

--- app/components/MainMenu.vue
+++ app/components/MainMenu.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useAsideStore } from '~/stores/website';
+import { menuList } from '~/data/MenuEntries';
 const store = useAsideStore()
 
 
@@ -9,20 +10,12 @@ function saveClick() {
   throttle()
 }
 
-// list of stuff sold - drinks, confectionaries, desserts, extras etc -, child of 'Menu' element from array above
-const desktopMenuItems = [
-  { id: 1, title: 'Item 1', icon: 'lucide:dice-1' },
-  { id: 2, title: 'Item 2', icon: 'lucide:dice-2' },
-  { id: 3, title: 'Item 3', icon: 'lucide:dice-3' },
-  { id: 4, title: 'Item 4', icon: 'lucide:dice-4' },
-]
 </script>
 <template>
   <!-- mobile/tablet -->
   <nav class="flex desktop:hidden fixed z-10 bottom-0 gap-4 w-11/12 m-[0_auto] justify-around items-center min-h-[60px] rounded-xl">
     <ul class="--menu-left flex gap-1 flex-1">
-      <BaseButton class="bg-espresso rounded-md text-beige">Item</BaseButton>
-      <BaseButton class="bg-espresso rounded-md text-beige">Item</BaseButton>
+      <NuxtLink class="bg-espresso rounded-md text-beige" to="/menu">Menu</NuxtLink>
     </ul>
 
     <NuxtImg width="50" height="50" src="/maplecorner_logo.webp" class="rounded-full shadow-[0px_0px_10px_1px_var(--color-beige)]" />
@@ -42,9 +35,11 @@ const desktopMenuItems = [
       <span class="--nav-header text-5xl font-bold">Maple Corner</span>
     </div>
     <ul class="flex gap-1 flex-1 justify-around items-center">
-      <li v-for="item in desktopMenuItems" :key="item.id" class="flex items-center justify-center">
-        <Icon :name="item.icon" size="45" />
-        <span>{{ item.title }}</span>
+      <li v-for="item in menuList" :key="item.id" class="flex items-center justify-center">
+        <NuxtLink class="flex gap-2 items-center" :to="item.url">
+          <Icon :name="item.icon" size="40" />
+          <span>{{ item.title }}</span>
+        </NuxtLink>
       </li>
     </ul>
   </nav>
--- app/pages/menu.vue
+++ app/pages/menu.vue
@@ -36,18 +36,18 @@ function capitalize(t: string): string {
 
 </script>
 <template>
-  <main>
+  <div class="outer">
     <form class="filters">
-      <ul class="flex h-fit w-full flex-col gap-4">
-        <li class="bg-beige rounded-md flex gap-2" v-for="item in filterCats" :key="item">
+      <ul class="flex h-fit w-full flex-col gap-2">
+        <li class="bg-beige rounded-md flex gap-2 px-2" v-for="item in filterCats" :key="item">
           <input :key="item" type="checkbox" :id="'filter' + item" :name="item ?? 'empty'"><label :for="'filter' + item">{{ capitalize(item ?? 'Incorrect value') }}</label>
         </li>
       </ul>
       <button></button>
     </form>
-    <div class="content">
-      <ul class="product_list flex flex-wrap">
-        <li class="flex flex-col items-center gap-4 max-w-1/2 basis-1/2 max-h-fit flex-1" v-for="item in data">
+    <section class="content">
+      <ul class="product_list grid grid-cols-3 gap-8">
+        <li class="flex flex-col items-center gap-4 flex-1 bg-espresso rounded-sm text-beige p-4" v-for="item in data">
           <NuxtImg :src="item.img ?? '/product_card_placeholder.jpg'" width="300" height="300" />
           <div class="flex flex-col max-w-fit justify-center gap-2 w-2/3">
             <h3>{{ item.name }}</h3>
@@ -56,18 +56,18 @@ function capitalize(t: string): string {
         </li>
       </ul>
 
-    </div>
-  </main>
+    </section>
+  </div>
 </template>
 
 <style scoped>
 .filters, .content {
   display: grid;
-  border: 2px solid lightblue;
 }
 
-main {
+.outer {
   display: grid;
   grid-template-columns: 2fr 10fr;
+  gap: 24px;
 }
 </style>

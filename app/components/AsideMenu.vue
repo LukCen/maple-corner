29b5a1a25bd75390267634329a3af8f3515dc6b1
lbjcena <lbjcena@gmail.com>
2025-09-17 23:45:26 +0200

    aside position changed to fixed - stays in viewport properly

--- app/components/AsideMenu.vue
+++ app/components/AsideMenu.vue
@@ -7,14 +7,13 @@ const store = useAsideStore()
 
 <!-- off-\screen menu that slides into view on mobile - non-rendering on desktop and wide screens -->
 <template>
-  <aside v-if="store.isActive === true" class=" flex flex-col h-[calc(100vh-80px)] scale-100 duration-500 px-4 py-1 gap-2 top-0 left-0 w-screen absolute
+  <aside v-if="store.isActive === true" class="flex flex-col h-[calc(100vh-80px)] scale-100 duration-500 px-4 py-1 gap-2 top-0 left-0 w-screen fixed
     bg-espresso/95 text-beige">
     <h2 class="font-bold">Menu</h2>
     <hr class="bg-beige" />
     <ul class="flex flex-col gap-8">
       <li v-for="item in menuList">
-        <NuxtLink :to="item.url"
-          class="flex gap-2 items-center h-fit bg-espresso/95 hover:text-espresso/95 duration-150">
+        <NuxtLink :to="item.url" class="flex gap-2 items-center h-fit bg-espresso/95 hover:text-espresso/95 duration-150">
           <Icon :name="item.icon" class="text-inherit duration-150" size="24" />
           <span class="font-semibold">{{ item.title }}</span>
         </NuxtLink>
